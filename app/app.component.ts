2b93f971ff36229608b3b3d651b7e58d8b1474bc
nagarajan <naga4042@gmail.com>
2016-04-14 20:07:03 +0530

    introducing objects

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,22 @@
 import {Component} from 'angular2/core';
 
+
+export class Hero {
+    id: number;
+    name: string;
+}
+
+
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero}}</h2>'
+    template: '<h1>{{title}}</h1><h2>{{hero.name}}</h2>'
 })
 
 export class AppComponent { 
 	public title = "Tour of Heros";
-	public hero = "windstorm";
+	public hero:Hero = {
+        id : 1,
+        name : 'Windstorm'
+    };
 }
+
